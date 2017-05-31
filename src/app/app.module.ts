be068d544a280352551867f138e808a64b2b94f9
Aubacca <aubacca@Peters-MacBook-Pro.local>
2017-05-31 16:59:14 +0200

    Added components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,33 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { NgReduxModule, NgRedux } from "@angular-redux/store";
+import { createLogger } from 'redux-logger';
+// import { rootReducer } from './reducers';
+
 import { AppComponent } from './app.component';
+import { GuestListComponent } from './components/guest-list/guest-list.component';
+import { GuestComponent } from './components/guest/guest.component';
+
+interface IAppState { /* ... */ };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuestListComponent,
+    GuestComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor (ngRedux: NgRedux<IAppState>) {
+//    ngRedux.configureStore(rootReducer, {}, [ createLogger() ]);
+  }
+}
---
+++ src/app/components/model/guests.ts
@@ -0,0 +1,242 @@
+export const GUESTS = [
+  {
+    "guid": "082ce160-a3e7-4bf4-ba4d-f0ed74f2b104",
+    "isActive": false,
+    "balance": "$1,477.45",
+    "picture": "http://placehold.it/32x32",
+    "age": 32,
+    "name": "Mitchell Avila",
+    "gender": "male",
+    "company": "VIRVA",
+    "email": "mitchellavila@virva.com",
+    "phone": "+1 (939) 460-3320",
+    "address": "468 Bliss Terrace, Kipp, American Samoa, 2045",
+    "about": "Ut non sunt qui esse nisi cupidatat officia Lorem eu pariatur do. Eiusmod do est laboris labore qui eu eu commodo minim. Sint proident minim sit incididunt. Irure pariatur sit duis veniam tempor adipisicing sint pariatur. Aliquip voluptate aute reprehenderit cupidatat aute excepteur elit eiusmod dolore aute deserunt.\r\n",
+    "registered": "2016-10-14T10:29:21 -02:00",
+    "tags": [
+      "non",
+      "occaecat",
+      "eiusmod",
+      "minim",
+      "dolore",
+      "do",
+      "officia"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Wiggins Oconnor"
+      },
+      {
+        "id": 1,
+        "name": "Eva Reed"
+      },
+      {
+        "id": 2,
+        "name": "Decker George"
+      }
+    ],
+    "greeting": "Hello, Mitchell Avila! You have 2 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "guid": "bfbc6e4a-740d-4b8c-934b-2219fd83afa0",
+    "isActive": true,
+    "balance": "$1,415.87",
+    "picture": "http://placehold.it/32x32",
+    "age": 38,
+    "name": "Cathy Cotton",
+    "gender": "female",
+    "company": "SNORUS",
+    "email": "cathycotton@snorus.com",
+    "phone": "+1 (828) 521-3930",
+    "address": "808 Pine Street, Wakarusa, Northern Mariana Islands, 4249",
+    "about": "Non do id sunt excepteur duis dolore commodo ipsum laborum non do dolor excepteur adipisicing. Nostrud enim voluptate dolor anim cillum sint aliquip culpa. Aliquip excepteur cillum ipsum reprehenderit commodo voluptate est cillum labore.\r\n",
+    "registered": "2015-07-02T03:28:28 -02:00",
+    "tags": [
+      "incididunt",
+      "voluptate",
+      "Lorem",
+      "quis",
+      "est",
+      "aliquip",
+      "cillum"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Elnora Mckee"
+      },
+      {
+        "id": 1,
+        "name": "Cherry Dominguez"
+      },
+      {
+        "id": 2,
+        "name": "Porter Carey"
+      }
+    ],
+    "greeting": "Hello, Cathy Cotton! You have 5 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "guid": "4525b559-f24d-496d-8cba-658b17492c24",
+    "isActive": true,
+    "balance": "$1,743.76",
+    "picture": "http://placehold.it/32x32",
+    "age": 29,
+    "name": "Cheri Lancaster",
+    "gender": "female",
+    "company": "ROCKLOGIC",
+    "email": "cherilancaster@rocklogic.com",
+    "phone": "+1 (956) 483-2853",
+    "address": "322 Nixon Court, Boomer, Missouri, 8297",
+    "about": "Fugiat nisi et incididunt fugiat exercitation duis nisi aliqua duis sit do amet ad ut. Id commodo et sunt duis adipisicing nisi incididunt nisi officia. Incididunt magna adipisicing cillum in incididunt reprehenderit in velit laboris voluptate laboris. Mollit irure magna non nostrud laboris.\r\n",
+    "registered": "2015-03-17T10:41:28 -01:00",
+    "tags": [
+      "aliqua",
+      "amet",
+      "eu",
+      "tempor",
+      "voluptate",
+      "nisi",
+      "adipisicing"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Kennedy Brooks"
+      },
+      {
+        "id": 1,
+        "name": "Howell Daniels"
+      },
+      {
+        "id": 2,
+        "name": "Allie Lynn"
+      }
+    ],
+    "greeting": "Hello, Cheri Lancaster! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "guid": "88acb824-44d5-4f88-a5f0-b0a04dad13aa",
+    "isActive": true,
+    "balance": "$2,185.37",
+    "picture": "http://placehold.it/32x32",
+    "age": 34,
+    "name": "Laurel Harrington",
+    "gender": "female",
+    "company": "APPLIDEC",
+    "email": "laurelharrington@applidec.com",
+    "phone": "+1 (819) 417-2885",
+    "address": "810 Williams Court, Roderfield, Alabama, 3348",
+    "about": "Eiusmod elit aliqua sunt nulla consectetur veniam sint dolore. Esse dolor aliqua aute veniam laboris tempor anim laborum ipsum dolore. Duis culpa cupidatat ipsum id pariatur dolore excepteur enim proident irure sit anim sunt. Nisi id proident quis dolor magna dolore occaecat enim ut eu deserunt. Cupidatat adipisicing ipsum laboris sunt. Cillum elit nisi culpa in nostrud qui consequat id sint non. Proident consectetur dolor fugiat deserunt eu mollit incididunt sunt.\r\n",
+    "registered": "2016-04-10T10:13:32 -02:00",
+    "tags": [
+      "dolor",
+      "sint",
+      "ex",
+      "labore",
+      "nulla",
+      "culpa",
+      "anim"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Carmen Frost"
+      },
+      {
+        "id": 1,
+        "name": "Jeanette Stanley"
+      },
+      {
+        "id": 2,
+        "name": "Rosalyn Gamble"
+      }
+    ],
+    "greeting": "Hello, Laurel Harrington! You have 3 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "guid": "096c343e-6f16-48b1-8994-333f30a972f9",
+    "isActive": false,
+    "balance": "$2,182.75",
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "name": "Hines Hernandez",
+    "gender": "male",
+    "company": "CHILLIUM",
+    "email": "hineshernandez@chillium.com",
+    "phone": "+1 (989) 496-2399",
+    "address": "479 Schenectady Avenue, Warren, Texas, 8495",
+    "about": "Laborum nulla ad magna incididunt eiusmod deserunt. Commodo anim adipisicing excepteur occaecat nulla id Lorem. Enim id tempor velit excepteur est. Incididunt aliquip duis ut incididunt esse culpa. Ea quis ea labore dolor duis culpa.\r\n",
+    "registered": "2014-09-10T04:17:23 -02:00",
+    "tags": [
+      "do",
+      "esse",
+      "duis",
+      "proident",
+      "adipisicing",
+      "Lorem",
+      "duis"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Deborah Miranda"
+      },
+      {
+        "id": 1,
+        "name": "Tyler Bentley"
+      },
+      {
+        "id": 2,
+        "name": "Cameron Webster"
+      }
+    ],
+    "greeting": "Hello, Hines Hernandez! You have 10 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "guid": "3444ef08-987b-4f72-b16f-662340ae94f5",
+    "isActive": true,
+    "balance": "$1,347.82",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "name": "Aline Pena",
+    "gender": "female",
+    "company": "UNQ",
+    "email": "alinepena@unq.com",
+    "phone": "+1 (803) 509-3910",
+    "address": "212 Russell Street, Wollochet, New Hampshire, 7094",
+    "about": "Laborum minim amet sit do in deserunt excepteur adipisicing. Nulla Lorem exercitation ad voluptate sint laborum in ad proident ex labore sint do elit. Aliquip incididunt cupidatat nostrud mollit. Irure in sunt minim Lorem veniam quis. Dolor in non laboris dolore.\r\n",
+    "registered": "2015-09-07T07:29:49 -02:00",
+    "tags": [
+      "deserunt",
+      "eu",
+      "nulla",
+      "id",
+      "eu",
+      "commodo",
+      "cupidatat"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Espinoza Montgomery"
+      },
+      {
+        "id": 1,
+        "name": "Lilian Bryan"
+      },
+      {
+        "id": 2,
+        "name": "Pitts Cobb"
+      }
+    ],
+    "greeting": "Hello, Aline Pena! You have 3 unread messages.",
+    "favoriteFruit": "banana"
+  }
+]
